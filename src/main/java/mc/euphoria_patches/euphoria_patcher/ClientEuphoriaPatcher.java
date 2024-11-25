b5d7baa73b9ed0a7c9daa02e26ee60b7e18082e2
SpacEagle17 <46494907+SpacEagle17@users.noreply.github.com>
2024-11-25 20:25:41 +0100

    Update ClientEuphoriaPatcher.java

--- src/main/java/mc/euphoria_patches/euphoria_patcher/ClientEuphoriaPatcher.java
+++ src/main/java/mc/euphoria_patches/euphoria_patcher/ClientEuphoriaPatcher.java
@@ -1,12 +1,12 @@
 package mc.euphoria_patches.euphoria_patcher;
 
-import mc.euphoria_patches.euphoria_patcher.util.ServerCheck;
+import mc.euphoria_patches.euphoria_patcher.util.ModLoaderSpecifics;
 import net.fabricmc.api.ModInitializer;
 
 public class ClientEuphoriaPatcher implements ModInitializer {
     @Override
     public void onInitialize() {
-        if(ServerCheck.check()) return;
+        if(ModLoaderSpecifics.serverCheck()) return;
         new EuphoriaPatcher();
     }
 }
