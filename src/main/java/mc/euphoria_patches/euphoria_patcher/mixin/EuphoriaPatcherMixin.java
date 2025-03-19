5ac694c8fd14387635f5efa1ed7bb182b4a2f651
SpacEagle17 <46494907+SpacEagle17@users.noreply.github.com>
2025-03-19 19:46:41 +0100

    even earlier initialization

--- src/main/java/mc/euphoria_patches/euphoria_patcher/mixin/EuphoriaPatcherMixin.java
+++ src/main/java/mc/euphoria_patches/euphoria_patcher/mixin/EuphoriaPatcherMixin.java
@@ -2,16 +2,15 @@ package mc.euphoria_patches.euphoria_patcher.mixin;
 
 import mc.euphoria_patches.euphoria_patcher.EuphoriaPatcher;
 import mc.euphoria_patches.euphoria_patcher.util.ModLoaderSpecifics;
-import net.minecraft.Bootstrap;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(Bootstrap.class)
+@Mixin(targets = "net.minecraft.client.main.Main")
 public class EuphoriaPatcherMixin {
-    @Inject(method = "initialize", at = @At("HEAD"))
-    private static void onBootstrap(CallbackInfo ci) {
+    @Inject(method = "main([Ljava/lang/String;)V", at = @At("HEAD"))
+    private static void onGameStart(String[] args, CallbackInfo ci) {
         if(ModLoaderSpecifics.serverCheck()) return;
         new EuphoriaPatcher();
     }
