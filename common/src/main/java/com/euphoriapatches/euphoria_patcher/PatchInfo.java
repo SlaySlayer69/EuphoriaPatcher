50de5856338978030c013a2c5e05ac0e3399d818
SpacEagle17 <46494907+SpacEagle17@users.noreply.github.com>
2026-01-28 13:31:33 +0100

    EP 1.8.0

--- common/src/main/java/com/euphoriapatches/euphoria_patcher/PatchInfo.java
+++ common/src/main/java/com/euphoriapatches/euphoria_patcher/PatchInfo.java
@@ -1,8 +1,8 @@
 package com.euphoriapatches.euphoria_patcher;
 
 public class PatchInfo {
-    public static final String VERSION = "_r5.6.1";
-    public static final String PATCH_VERSION = "_1.7.8";
-    public static final String BASE_TAR_SHA256 = "85bedae6a1fc8cac5f24cbcca18950f850dc7867c30e7db2bb0e15cb63729fe5";
-    public static final int BASE_TAR_SIZE = 1440768;
+    public static final String VERSION = "_r5.7";
+    public static final String PATCH_VERSION = "_1.8.0";
+    public static final String BASE_TAR_SHA256 = "9d351e9a96d042d566fdde558c0da983a1ccaf5f9e8eaea419ccc3c790d17b16";
+    public static final int BASE_TAR_SIZE = 1525760;
 }
