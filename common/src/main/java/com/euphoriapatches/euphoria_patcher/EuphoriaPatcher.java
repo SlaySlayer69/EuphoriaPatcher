5d580636f67f40d49864fd4c29a63defd5c2dfe2
SpacEagle17 <46494907+SpacEagle17@users.noreply.github.com>
2026-04-30 23:13:45 +0200

    move to a new class

--- common/src/main/java/com/euphoriapatches/euphoria_patcher/EuphoriaPatcher.java
+++ common/src/main/java/com/euphoriapatches/euphoria_patcher/EuphoriaPatcher.java
@@ -12,7 +12,6 @@ import com.euphoriapatches.euphoria_patcher.services.*;
 import com.euphoriapatches.euphoria_patcher.services.ShaderDetector.ShaderInfo;
 import com.euphoriapatches.euphoria_patcher.util.*;
 
-import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.*;
@@ -57,7 +56,7 @@ public class EuphoriaPatcher {
         // Initialize the logger
         loggerInstance = new EuphoriaLogger();
         loggerInstance.checkErrorLogFileAndAddSeparator();
-        JarLauncher.touch(); // Call to prevent minimizers from removing the GUI code.
+        JarLauncher.touch(); // Call to prevent minimization from removing the GUI code.
 
         ConfigHandler.configStuff();
 
@@ -224,7 +223,7 @@ public class EuphoriaPatcher {
         Path shader = findInstalledShaderPath(baseFile, installedDir);
 
         if (shader != null) {
-            applyShaderModifications(shader, styleUnbound, styleReimagined, isAlreadyInstalled);
+            BootShaderModificator.applyShaderModifications(shader, styleUnbound, styleReimagined, isAlreadyInstalled);
             displayFinalMessage();
         } else {
             debugLog("No valid shader path found for thank you message");
@@ -243,126 +242,6 @@ public class EuphoriaPatcher {
         }
 
         return shaderDetector.findPatchedShaderDirectory();
-    }
-
-    private void applyShaderModifications(Path shader, boolean styleUnbound, boolean styleReimagined, boolean isAlreadyInstalled) {
-        applyUpdateNotification(shader, styleUnbound, styleReimagined);
-        applyCompatibilityModifications(shader, styleUnbound, styleReimagined);
-        applyDeveloperModifications(shader, styleUnbound, styleReimagined);
-        namingService.createAlternativeShaderNames(shader, isAlreadyInstalled, ConfigHandler.alternativeShaderNames);
-
-        // Initialize properties watcher if auto-merge is enabled
-        if (ConfigHandler.autoMergeBlockProperties) {
-            debugLog("Auto-merge block properties is enabled, starting properties watcher");
-            PropertiesWatcher.startWatcher();
-        } else {
-            debugLog("Auto-merge block properties is disabled, not starting properties watcher");
-        }
-    }
-
-    private void applyUpdateNotification(Path shader, boolean styleUnbound, boolean styleReimagined) {
-        if (!UpdateChecker.shouldUserUpdate()) {
-            return;
-        }
-
-        boolean isOculus = ShaderLoader.getShaderLoader().equals(ShaderLoader.OCULUS);
-        boolean isOptifine = ShaderLoader.getShaderLoader().equals(ShaderLoader.OPTIFINE);
-
-        String newVersionText = "value.info19.0=§c" + PATCH_VERSION.replace("_", "") + " §r->§a " + UpdateChecker.getNewModVersion();
-        if (isOculus || isOptifine && !ShaderLoader.isMinecraftVersionAtLeast("1.21.1")) {
-            newVersionText = "value.info19.0=§c" + PATCH_VERSION.replace("_", "") + " -> " + UpdateChecker.getNewModVersion();
-        }
-
-        try {
-            // Shaders Properties Modifications
-            ModifyPatchedShaderpacks.modifyFiles(shader, styleUnbound, styleReimagined, SHADERS_PROPERTIES_LOCATION, null, "screen=<empty> <empty>", "screen=info19 info20");
-            ModifyPatchedShaderpacks.modifyFiles(shader, styleUnbound, styleReimagined, LANG_LOCATION, ".lang", "value\\.info19\\.0=.*", newVersionText);
-
-            // Iris Shader Folder Mod Description
-            // 4 \\\\ have to be used to represent 1 \ in the final JSON due to multiple layers of escaping
-            String shaderDescriptionText = "\\\\u00A7cComplementary Shaders " + VERSION.replace("_", "") + "\\\\u00A7r + \\\\u00A7dEuphoria Patches " + PATCH_VERSION.replace("_", "") + "\\\\u00A7r Complementary add-on by SpacEagle17 extending it with many more unique optional features and settings.\\\\nDev versions: \\\\u00A7dwww.euphoriapatches.com/support\\\\u00A7r";
-
-            ModifyPatchedShaderpacks.modifyFiles(shader, styleUnbound, styleReimagined, "shaders/pack.json", null,
-            "\"shaderDescription\":.*", "\"shaderDescription\": \"" + "\\\\u00A7aUPDATE AVAILABLE!\\\\u00A7r\\\\nCurrent version: \\\\u00A7c" + PATCH_VERSION.replace("_", "") + "\\\\u00A7r -> " + "New version: \\\\u00A7a" + UpdateChecker.getNewModVersion() + "\\\\u00A7r\\\\n----------------\\\\n" + shaderDescriptionText + "\",");
-
-        } catch (IOException e) {
-            log(3, 0, "Could not modify the shader to show the user that a new version is available" + e.getMessage());
-        }
-    }
-
-    private void applyCompatibilityModifications(Path shader, boolean styleUnbound, boolean styleReimagined) {
-        boolean isOptifine = ShaderLoader.getShaderLoader().equals(ShaderLoader.OPTIFINE);
-        boolean isAngelica = ShaderLoader.getShaderLoader().equals(ShaderLoader.ANGELICA);
-        boolean isMacOS = System.getProperty("os.name").toLowerCase(Locale.ROOT).contains("mac");
-        boolean isPhotonicsInstalled = ModChecker.isModPresent(ModChecker.ModClasses.PHOTONICS);
-
-        // This changes the popular settings profile dynamically
-        if (isOptifine || isMacOS) {
-            removeColoredLighting(shader, styleUnbound, styleReimagined);
-            removeEndCrystalVortex(shader, styleUnbound, styleReimagined);
-            removeDragonDeathEffect(shader, styleUnbound, styleReimagined);
-            removeEndPortalBeam(shader, styleUnbound, styleReimagined);
-        } else if (isAngelica) {
-            removeEndCrystalVortex(shader, styleUnbound, styleReimagined);
-            removeDragonDeathEffect(shader, styleUnbound, styleReimagined);
-            removeEndPortalBeam(shader, styleUnbound, styleReimagined);
-        } else if (isPhotonicsInstalled) {
-            removeColoredLighting(shader, styleUnbound, styleReimagined);
-        }
-    }
-
-    private void removeColoredLighting(Path shader, boolean styleUnbound, boolean styleReimagined) {
-        try {
-            // Change COLORED_LIGHTING from 192 to 0
-            ModifyPatchedShaderpacks.modifyFiles(shader, styleUnbound, styleReimagined, SHADERS_PROPERTIES_LOCATION, null,
-                "(profile2\\.POPULAR\\s+=\\s+.*?COLORED_LIGHTING=)192(\\s+.*)", "$10  $2");
-            debugLog("Removed COLORED_LIGHTING=192 from POPULAR profile");
-        } catch (IOException e) {
-            log(3, 0, "Could not remove COLORED_LIGHTING=192 from POPULAR profile: " + e.getMessage());
-        }
-    }
-
-    private void removeEndCrystalVortex(Path shader, boolean styleUnbound, boolean styleReimagined) {
-        try {
-            // Change END_CRYSTAL_VORTEX from 3 to 0
-            ModifyPatchedShaderpacks.modifyFiles(shader, styleUnbound, styleReimagined, SHADERS_PROPERTIES_LOCATION, null,
-                "(profile2\\.POPULAR\\s+=\\s+.*?END_CRYSTAL_VORTEX=)3(\\s+.*)", "$10$2");
-            debugLog("Removed END_CRYSTAL_VORTEX=3 from POPULAR profile");
-        } catch (IOException e) {
-            log(3, 0, "Could not remove END_CRYSTAL_VORTEX=3 from POPULAR profile: " + e.getMessage());
-        }
-    }
-
-    private void removeEndPortalBeam(Path shader, boolean styleUnbound, boolean styleReimagined) {
-        try {
-            // Change END_PORTAL_BEAM to !END_PORTAL_BEAM
-            ModifyPatchedShaderpacks.modifyFiles(shader, styleUnbound, styleReimagined, SHADERS_PROPERTIES_LOCATION, null,
-                    "(profile2\\.POPULAR\\s+=\\s+.*?)\\s+END_PORTAL_BEAM(\\s+.*)", "$1 !END_PORTAL_BEAM$2");
-            debugLog("Removed END_PORTAL_BEAM from POPULAR profile");
-        } catch (IOException e) {
-            log(3, 0, "Could not remove END_PORTAL_BEAM=1 from POPULAR profile: " + e.getMessage());
-        }
-    }
-
-    private void removeDragonDeathEffect(Path shader, boolean styleUnbound, boolean styleReimagined) {
-        try {
-            // Change DRAGON_DEATH_EFFECT=1 to DRAGON_DEATH_EFFECT=0
-            ModifyPatchedShaderpacks.modifyFiles(shader, styleUnbound, styleReimagined, SHADERS_PROPERTIES_LOCATION, null,
-                    "(profile2\\.POPULAR\\s+=\\s+.*?)\\s+DRAGON_DEATH_EFFECT=1(\\s+.*)", "$1 DRAGON_DEATH_EFFECT=0$2");
-            debugLog("Removed DRAGON_DEATH_EFFECT=1 from POPULAR profile");
-        } catch (IOException e) {
-            log(3, 0, "Could not remove DRAGON_DEATH_EFFECT=1 from POPULAR profile: " + e.getMessage());
-        }
-    }
-
-    private void applyDeveloperModifications(Path shader, boolean styleUnbound, boolean styleReimagined) {
-        if (isSpacEagle()) {
-            try {
-                ModifyPatchedShaderpacks.modifyFiles(shader, styleUnbound, styleReimagined, SHADER_MYFILE_LOCATION, null, "\\/\\/ Developed by SpacEagle17", "#define SPACEAGLE17");
-            } catch (IOException e) {
-                log(3, 0, "Could not modify the shader for SpacEagle17" + e.getMessage());
-            }
-        }
     }
 
     private void displayFinalMessage() {
---
+++ common/src/main/java/com/euphoriapatches/euphoria_patcher/util/BootShaderModificator.java
@@ -0,0 +1,144 @@
+package com.euphoriapatches.euphoria_patcher.util;
+
+import com.euphoriapatches.euphoria_patcher.EuphoriaPatcher;
+import com.euphoriapatches.euphoria_patcher.config.ConfigHandler;
+import com.euphoriapatches.euphoria_patcher.features.ModifyPatchedShaderpacks;
+import com.euphoriapatches.euphoria_patcher.features.properties.PropertiesWatcher;
+import com.euphoriapatches.euphoria_patcher.integration.ShaderLoader;
+import com.euphoriapatches.euphoria_patcher.logging.EuphoriaLogger;
+import com.euphoriapatches.euphoria_patcher.services.ShaderNamingService;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.Locale;
+
+public class BootShaderModificator {
+
+    private static void debugLog(String message) {
+        EuphoriaLogger.debugLog("[BootShaderModificator] " + message);
+    }
+
+    public static void applyShaderModifications(Path shader, boolean styleUnbound, boolean styleReimagined, boolean isAlreadyInstalled) {
+        applyUpdateNotification(shader, styleUnbound, styleReimagined);
+        applyCompatibilityModifications(shader, styleUnbound, styleReimagined);
+        applyDeveloperModifications(shader, styleUnbound, styleReimagined);
+
+        EuphoriaPatcher instance = EuphoriaPatcher.getInstance();
+        ShaderNamingService namingService = instance.getNamingService();
+
+        namingService.createAlternativeShaderNames(shader, isAlreadyInstalled, ConfigHandler.alternativeShaderNames);
+
+        // Initialize properties watcher if auto-merge is enabled
+        if (ConfigHandler.autoMergeBlockProperties) {
+            debugLog("Auto-merge block properties is enabled, starting properties watcher");
+            PropertiesWatcher.startWatcher();
+        } else {
+            debugLog("Auto-merge block properties is disabled, not starting properties watcher");
+        }
+    }
+
+    private static void applyUpdateNotification(Path shader, boolean styleUnbound, boolean styleReimagined) {
+        if (!UpdateChecker.shouldUserUpdate()) {
+            return;
+        }
+
+        boolean isOculus = ShaderLoader.getShaderLoader().equals(ShaderLoader.OCULUS);
+        boolean isOptifine = ShaderLoader.getShaderLoader().equals(ShaderLoader.OPTIFINE);
+
+        String newVersionText = "value.info19.0=§c" + EuphoriaPatcher.PATCH_VERSION.replace("_", "") + " §r->§a " + UpdateChecker.getNewModVersion();
+        if (isOculus || isOptifine && !ShaderLoader.isMinecraftVersionAtLeast("1.21.1")) {
+            newVersionText = "value.info19.0=§c" + EuphoriaPatcher.PATCH_VERSION.replace("_", "") + " -> " + UpdateChecker.getNewModVersion();
+        }
+
+        try {
+            // Shaders Properties Modifications
+            ModifyPatchedShaderpacks.modifyFiles(shader, styleUnbound, styleReimagined, EuphoriaPatcher.SHADERS_PROPERTIES_LOCATION, null, "screen=<empty> <empty>", "screen=info19 info20");
+            ModifyPatchedShaderpacks.modifyFiles(shader, styleUnbound, styleReimagined, EuphoriaPatcher.LANG_LOCATION, ".lang", "value\\.info19\\.0=.*", newVersionText);
+
+            // Iris Shader Folder Mod Description
+            // 4 \\\\ have to be used to represent 1 \ in the final JSON due to multiple layers of escaping
+            String shaderDescriptionText = "\\\\u00A7cComplementary Shaders " + EuphoriaPatcher.VERSION.replace("_", "") + "\\\\u00A7r + \\\\u00A7dEuphoria Patches " + EuphoriaPatcher.PATCH_VERSION.replace("_", "") + "\\\\u00A7r Complementary add-on by SpacEagle17 extending it with many more unique optional features and settings.\\\\nDev versions: \\\\u00A7dwww.euphoriapatches.com/support\\\\u00A7r";
+
+            ModifyPatchedShaderpacks.modifyFiles(shader, styleUnbound, styleReimagined, "shaders/pack.json", null,
+                    "\"shaderDescription\":.*", "\"shaderDescription\": \"" + "\\\\u00A7aUPDATE AVAILABLE!\\\\u00A7r\\\\nCurrent version: \\\\u00A7c" + EuphoriaPatcher.PATCH_VERSION.replace("_", "") + "\\\\u00A7r -> " + "New version: \\\\u00A7a" + UpdateChecker.getNewModVersion() + "\\\\u00A7r\\\\n----------------\\\\n" + shaderDescriptionText + "\",");
+
+        } catch (IOException e) {
+            EuphoriaPatcher.log(3, 0, "Could not modify the shader to show the user that a new version is available" + e.getMessage());
+        }
+    }
+
+    private static void applyCompatibilityModifications(Path shader, boolean styleUnbound, boolean styleReimagined) {
+        boolean isOptifine = ShaderLoader.getShaderLoader().equals(ShaderLoader.OPTIFINE);
+        boolean isAngelica = ShaderLoader.getShaderLoader().equals(ShaderLoader.ANGELICA);
+        boolean isMacOS = System.getProperty("os.name").toLowerCase(Locale.ROOT).contains("mac");
+        boolean isPhotonicsInstalled = ModChecker.isModPresent(ModChecker.ModClasses.PHOTONICS);
+
+        // This changes the popular settings profile dynamically
+        if (isOptifine || isMacOS) {
+            removeColoredLighting(shader, styleUnbound, styleReimagined);
+            removeEndCrystalVortex(shader, styleUnbound, styleReimagined);
+            removeDragonDeathEffect(shader, styleUnbound, styleReimagined);
+            removeEndPortalBeam(shader, styleUnbound, styleReimagined);
+        } else if (isAngelica) {
+            removeEndCrystalVortex(shader, styleUnbound, styleReimagined);
+            removeDragonDeathEffect(shader, styleUnbound, styleReimagined);
+            removeEndPortalBeam(shader, styleUnbound, styleReimagined);
+        } else if (isPhotonicsInstalled) {
+            removeColoredLighting(shader, styleUnbound, styleReimagined);
+        }
+    }
+
+    private static void removeColoredLighting(Path shader, boolean styleUnbound, boolean styleReimagined) {
+        try {
+            // Change COLORED_LIGHTING from 192 to 0
+            ModifyPatchedShaderpacks.modifyFiles(shader, styleUnbound, styleReimagined, EuphoriaPatcher.SHADERS_PROPERTIES_LOCATION, null,
+                    "(profile2\\.POPULAR\\s+=\\s+.*?COLORED_LIGHTING=)192(\\s+.*)", "$10  $2");
+            debugLog("Removed COLORED_LIGHTING=192 from POPULAR profile");
+        } catch (IOException e) {
+            EuphoriaPatcher.log(3, 0, "Could not remove COLORED_LIGHTING=192 from POPULAR profile: " + e.getMessage());
+        }
+    }
+
+    private static void removeEndCrystalVortex(Path shader, boolean styleUnbound, boolean styleReimagined) {
+        try {
+            // Change END_CRYSTAL_VORTEX from 3 to 0
+            ModifyPatchedShaderpacks.modifyFiles(shader, styleUnbound, styleReimagined, EuphoriaPatcher.SHADERS_PROPERTIES_LOCATION, null,
+                    "(profile2\\.POPULAR\\s+=\\s+.*?END_CRYSTAL_VORTEX=)3(\\s+.*)", "$10$2");
+            debugLog("Removed END_CRYSTAL_VORTEX=3 from POPULAR profile");
+        } catch (IOException e) {
+            EuphoriaPatcher.log(3, 0, "Could not remove END_CRYSTAL_VORTEX=3 from POPULAR profile: " + e.getMessage());
+        }
+    }
+
+    private static void removeEndPortalBeam(Path shader, boolean styleUnbound, boolean styleReimagined) {
+        try {
+            // Change END_PORTAL_BEAM to !END_PORTAL_BEAM
+            ModifyPatchedShaderpacks.modifyFiles(shader, styleUnbound, styleReimagined, EuphoriaPatcher.SHADERS_PROPERTIES_LOCATION, null,
+                    "(profile2\\.POPULAR\\s+=\\s+.*?)\\s+END_PORTAL_BEAM(\\s+.*)", "$1 !END_PORTAL_BEAM$2");
+            debugLog("Removed END_PORTAL_BEAM from POPULAR profile");
+        } catch (IOException e) {
+            EuphoriaPatcher.log(3, 0, "Could not remove END_PORTAL_BEAM=1 from POPULAR profile: " + e.getMessage());
+        }
+    }
+
+    private static void removeDragonDeathEffect(Path shader, boolean styleUnbound, boolean styleReimagined) {
+        try {
+            // Change DRAGON_DEATH_EFFECT=1 to DRAGON_DEATH_EFFECT=0
+            ModifyPatchedShaderpacks.modifyFiles(shader, styleUnbound, styleReimagined, EuphoriaPatcher.SHADERS_PROPERTIES_LOCATION, null,
+                    "(profile2\\.POPULAR\\s+=\\s+.*?)\\s+DRAGON_DEATH_EFFECT=1(\\s+.*)", "$1 DRAGON_DEATH_EFFECT=0$2");
+            debugLog("Removed DRAGON_DEATH_EFFECT=1 from POPULAR profile");
+        } catch (IOException e) {
+            EuphoriaPatcher.log(3, 0, "Could not remove DRAGON_DEATH_EFFECT=1 from POPULAR profile: " + e.getMessage());
+        }
+    }
+
+    private static void applyDeveloperModifications(Path shader, boolean styleUnbound, boolean styleReimagined) {
+        if (EuphoriaPatcher.isSpacEagle()) {
+            try {
+                ModifyPatchedShaderpacks.modifyFiles(shader, styleUnbound, styleReimagined, EuphoriaPatcher.SHADER_MYFILE_LOCATION, null, "\\/\\/ Developed by SpacEagle17", "#define SPACEAGLE17");
+            } catch (IOException e) {
+                EuphoriaPatcher.log(3, 0, "Could not modify the shader for SpacEagle17" + e.getMessage());
+            }
+        }
+    }
+}
