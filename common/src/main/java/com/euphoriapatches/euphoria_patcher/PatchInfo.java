0f19292a127d5adce9312ae66a31a46d168be1f7
SpacEagle17 <46494907+SpacEagle17@users.noreply.github.com>
2026-09-02 13:06:50 +0200

    1.10.0

--- common/src/main/java/com/euphoriapatches/euphoria_patcher/PatchInfo.java
+++ common/src/main/java/com/euphoriapatches/euphoria_patcher/PatchInfo.java
@@ -1,8 +1,8 @@
 package com.euphoriapatches.euphoria_patcher;
 
 public class PatchInfo {
-    public static final String VERSION = "_r5.8.1";
-    public static final String PATCH_VERSION = "_1.9.3";
-    public static final String BASE_TAR_SHA256 = "b73e323a886fe9f67df2c38528ce77e2d73e0af7d0dd30515b516b59e249df24";
-    public static final int BASE_TAR_SIZE = 1618944;
+    public static final String VERSION = "_r5.9";
+    public static final String PATCH_VERSION = "_1.10.0";
+    public static final String BASE_TAR_SHA256 = "5e627575eb31710c502a4680860f4fd62cf74a6982c2194a9844395bfd3a2b5c";
+    public static final int BASE_TAR_SIZE = 1643520;
 }
